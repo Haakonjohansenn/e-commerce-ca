cd9c52887ee8322fee99e95e67782d6c08f1469d
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2024-04-07 13:21:40 +0200

    nextconfig

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,10 @@
+// @ts-check
+ 
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        fs: false,
-      };
-    }
-
-    return config;
-  },
-};
-
-module.exports = nextConfig;
+  /* config options here */
+}
+ 
+export default nextConfig
