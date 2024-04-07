6ab22fdc31e80704ef71ad388fe4f2d739a8a4a3
Haakonjohansenn <111745800+Haakonjohansenn@users.noreply.github.com>
2024-04-07 14:01:59 +0200

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
-// @ts-check
- 
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-}
- 
-export default nextConfig
+  images: {
+    domains: ['static.noroff.dev'],
+  },
+  experimental: {
+    appDir: true,
+  },
+};
+
+export default nextConfig;
